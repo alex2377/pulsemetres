f5166c67f35a78f761398640d1dc9996b6450cb0
Aleksei <23leha77@mail.ru>
2021-09-25 10:17:46 +0300

    add_animations

--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -8,15 +8,15 @@ require_once('phpmailer/PHPMailerAutoload.php');
 $mail = new PHPMailer();
 $mail->CharSet = 'utf-8';
 
-// $mail->SMTPDebug = 3;                               // Enable verbose debug output
+//$mail->SMTPDebug = 1;                               // Enable verbose debug output
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = '23lehaverycrut@gmail.com';                 // Наш логин
 $mail->Password = 'ronaldinho11';                           // Наш пароль от ящика
-$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 587;                                    // TCP port to connect to
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465;                                    // TCP port to connect to
  
 $mail->setFrom('23lehaverycrut@gmail.com', 'Pulse');   // От кого письмо 
 $mail->addAddress('leha237@yandex.ru');     // Add a recipient Куда приходит
